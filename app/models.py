40b27ddc42de5dd8a4e689e43d58d2ee4d5c8d28
mikefromru <googlockgood@gmail.com>
2023-09-23 16:49:01 +0300

    Made first, last names require

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
 
+    first_name= models.CharField(null=False, blank=False, max_length=100)
+    last_name = models.CharField(null=False, blank=False, max_length=100)
     birthday = models.DateField(null=True, blank=True)
 
     def __str__(self):
