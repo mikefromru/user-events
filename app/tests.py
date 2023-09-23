102aca97585259bb269ae1a7da2faf2981d1ef79
mikefromru <googlockgood@gmail.com>
2023-09-23 17:45:59 +0300

    Added a user in setUp in test

--- app/tests.py
+++ app/tests.py
@@ -1,9 +1,22 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from .models import Event
+
+User = get_user_model()
 
 class TestUser(TestCase):
 
+    def setUp(self):
+        self.user_1 = User.objects.create_user(username='user_1', password='qwerty')
+
     def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(username='user_1', password='qwerty')
+        user = User.objects.create_user(username='user_2', password='qwerty')
         self.assertFalse(user.is_superuser)
+        count_users = User.objects.all().count()
+        self.assertEqual(count_users, 2)
+    
+
+
+class TestEvent(TestCase):
+    pass
+
