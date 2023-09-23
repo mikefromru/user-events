a1219f8ff2bc0903d9af037f999a728e7d547f1f
mikefromru <googlockgood@gmail.com>
2023-09-23 17:16:12 +0300

    Made first test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class TestUser(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='user_1', password='qwerty')
+        self.assertFalse(user.is_superuser)
