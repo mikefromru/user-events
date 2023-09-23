9bad00ddce9b379cb83c7c43edb9914a543b0eda
mikefromru <googlockgood@gmail.com>
2023-09-23 18:15:40 +0300

    Added test_create_even test

--- app/tests.py
+++ app/tests.py
@@ -4,19 +4,20 @@ from .models import Event
 
 User = get_user_model()
 
-class TestUser(TestCase):
+class TestApp(TestCase):
 
     def setUp(self):
         self.user_1 = User.objects.create_user(username='user_1', password='qwerty')
+        self.user_2 = User.objects.create_user(username='user_2', password='qwerty')
 
     def test_create_user(self):
-        user = User.objects.create_user(username='user_2', password='qwerty')
-        self.assertFalse(user.is_superuser)
+        self.user_3 = User.objects.create_user(username='user_3', password='qwerty')
+        self.assertFalse(self.user_2.is_superuser)
         count_users = User.objects.all().count()
-        self.assertEqual(count_users, 2)
+        self.assertEqual(count_users, 3)
     
-
-
-class TestEvent(TestCase):
-    pass
-
+    def test_create_even(self):
+        self.event = Event.objects.create(title='My Event', body='Some text', creator=self.user_1)
+        self.event.participants.add(self.user_2)
+        self.assertEqual(self.event.creator, self.user_1)
+        self.assertEqual(self.event.participants.count(), 1)
