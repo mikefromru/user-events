354e038b8a1d288bf6778e633ec9dc4e5d8de252
mikefromru <googlockgood@gmail.com>
2023-09-24 01:30:44 +0300

    Added delete method for even

--- app/views.py
+++ app/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from .models import Event
 from .serializers import EventSerializer
 from django.contrib.auth import get_user_model
-
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 User = get_user_model()
 
@@ -12,9 +13,10 @@ class EventAPIView(APIView):
 
     # Get only auth user events
     def get(self, request):
+        # queryset = Event.objects.all()
         queryset = Event.objects.filter(creator=request.user)
         serializer = EventSerializer(queryset, many=True)
-        return Response(serializer.data, status=200)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     # Create user event and become participant
     def post(self, request, *args, **kwargs):
@@ -22,6 +24,13 @@ class EventAPIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EventDetailAPIView(APIView):
+
+    def delete(self, request, pk, format=None):
+        event = get_object_or_404(Event.objects.all(), pk=pk)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
