f2ede6875f042d853cc5a63621b496fe5a0b8a4f
mikefromru <googlockgood@gmail.com>
2023-09-24 22:30:54 +0300

    Splited api urls from common urls

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from . import views
+from .views import views
 
 urlpatterns = [
-    path('', views.EventAPIView.as_view(), name='event-user-list'),
-    path('events/', views.EventListAPIView.as_view(), name='event-list'),
-    path('event/<int:pk>/', views.EventDetailAPIView.as_view(), name='event'),
+    path('', views.index, name='index'),
 ]
-
