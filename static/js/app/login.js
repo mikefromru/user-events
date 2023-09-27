aadd6475e2c00a8ba298be02bb74bc301b4c8c83
mikefromru <googlockgood@gmail.com>
2023-09-28 00:13:29 +0300

    Added login page with vue.js

---
+++ static/js/app/login.js
@@ -0,0 +1,36 @@
+console.log('fuck you motherfucker')
+new Vue({
+    // delimiters: ['[[', ']]'],
+    el: '#login-app',
+    data() {
+        return {
+            username: '',
+            password: '',
+        }
+    },
+    methods: {
+        myclick() {
+            console.log('click on button')
+        },
+        login() {
+            payload = {'login': 'admin', 'password': 'admin'}
+            axios.post('accounts/login/', body=payload)
+            .then(response => (
+                // this.lst = response.data
+                console.log(response.data)
+            ))
+            .catch(function (error) {
+                if (error.response) {
+                    console.log('error.response.data')
+                    if (error.response.status == 401) {
+                        console.log('401')
+                        // window.location.href = '/login/';
+
+                    }
+
+                }
+            })
+        },
+    }
+})
+
