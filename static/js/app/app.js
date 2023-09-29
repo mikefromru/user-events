5830da000d26fb4d7d5da568d0e048bc0b745bca
mikefromru <googlockgood@gmail.com>
2023-09-29 23:22:33 +0300

    Added func for become or stop being participant of event

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -7,23 +7,46 @@ new Vue({
             detail_event: {},
             user_events: [],
             flag: false,
+            user_info: {},
+            show_detail_even_block: true,
+            show_user_info: false,
         }
     },
     methods: {
+        get_participate_or_stop(x) {
+            axios.defaults.xsrfCookieName = 'csrftoken'
+            axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+            id_event = this.detail_event.id + '/'
+            axios.patch('/api/v1/app/event/' + id_event)
+                .then(response => (
+                    this.detail_event = response.data
+                ))
+
+        },
+        close() {
+            // hide user info block and show an event
+            this.show_user_info = false
+            this.show_detail_even_block = true
+        },
+        get_user_info(user) {
+            this.user_info = user
+            this.show_detail_even_block = false
+            this.show_user_info = true
+        },
         get_event(ev) {
             this.detail_event = ev
             this.flag = true
-            
+            this.close()
+
         },
         logout() {
-            console.log('logout')
             axios.defaults.xsrfCookieName = 'csrftoken'
             axios.defaults.xsrfHeaderName = 'X-CSRFToken'
-            axios.post('/accounts/logout/', {'revoke_token': true})
-            .then(response => (
-                console.log(response.data),
-                window.location.reload()
-            ))
+            axios.post('/accounts/logout/', { 'revoke_token': true })
+                .then(response => (
+                    console.log(response.data),
+                    window.location.reload()
+                ))
         }
     },
     mounted() {
