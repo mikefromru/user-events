f31181346514259c38186a9dad897f947630492b
mikefromru <googlockgood@gmail.com>
2023-10-06 03:17:44 +0300

    Added func to generate random SECRET_KEY

--- app/models.py
+++ app/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.conf import settings
 
-class Event(models.Model):
 
+class Event(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_events')
-    participants = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='participated_events')
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='created_events',
+    )
+    participants = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name='participated_events')
 
     def __str__(self):
         return self.title
-
--- project/settings/base.py
+++ project/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 env.read_env()
@@ -24,7 +25,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+from django.core.management.utils import get_random_secret_key
+
+# SECRET_KEY = env('SECRET_KEY')
+# if not env('SECRET_KEY'):
+
+def get_env_secret_key():
+    if env('SECRET_KEY'):
+      return env('SECRET_KEY')
+    else:
+      secret_key = get_random_secret_key
+      os.environ["SECRET_KEY"] = secret_key
+      return secret_key
+    
+SECRET_KEY = get_env_secret_key()
+
 
 # Application definition
 
