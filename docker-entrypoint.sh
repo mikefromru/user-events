d90b370d2942a69855f8890424bd0835aa7e8316
mikefromru <googlockgood@gmail.com>
2023-10-14 21:19:00 +0300

    Nginx Docker-compose fixed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Collect static files
-echo "Collect static files"
+echo "<<<<<<<<<< Collect static files >>>>>>>>>>"
 python manage.py collectstatic --noinput
+#python manage.py collectstatic
 
 # Apply database migrations
-echo "Apply database migrations"
+echo "----------- Apply database migrations ----------"
 python manage.py migrate
 
 # Start server
--- project/settings/production.py
+++ project/settings/production.py
@@ -2,5 +2,5 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8000']
 
